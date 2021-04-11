3f04c912473a4c1b8dde5659472fe5d9c36d31a7
Akornefa Akyea <akornefa@Akornefas-MacBook-Pro.local>
2021-04-11 14:56:37 -0500

    Add array of Pokemon Data
    Added array of pokemon data including: name, height, ability and type. Added to scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-alert("Hello, World!");
-
-let favoriteFood = "lasagna";
-document.write(favoriteFood); 
+let pokemonList = [{name: 'Charmeleon', height: 1.1, abilites:['Blaze', 'Solar-power'], type: 'Fire'},
+{name: 'Squirtle', height: 0.5, ablities:['Rain-dish', 'Torrent'], type: 'water'}, {name: 'Butterfree',
+height: 1.1, abilites:['Compoundeyes', 'Tinted-lens'], type:['Bug','Flying']}];
