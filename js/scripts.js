158dbc71ca90657a138c257505d19eb1dc19f22b
Akornefa Akyea <akornefa@Akornefas-MacBook-Pro.local>
2021-04-12 17:34:21 -0500

    Updated formatting in scripts.js
    Included appropriate indentation in scripts.js. Also changed the 'type' property to an array for all objects.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
-let pokemonList = [{name: 'Charmeleon', height: 1.1, abilites:['Blaze', 'Solar-power'], type: 'Fire'},
-{name: 'Squirtle', height: 0.5, ablities:['Rain-dish', 'Torrent'], type: 'water'}, {name: 'Butterfree',
-height: 1.1, abilites:['Compoundeyes', 'Tinted-lens'], type:['Bug','Flying']}];
+let pokemonList = [{
+    name: 'Charmeleon',
+    height: 1.1,
+    abilites: ['Blaze', 'Solar-power'],
+    type: ['Fire'],
+  },
+  {
+    name: 'Squirtle',
+    height: 0.5,
+    ablities: ['Rain-dish', 'Torrent'],
+    type: 'water',
+  },
+  {
+    name: 'Butterfree',
+    height: 1.1,
+    abilites: ['Compoundeyes', 'Tinted-lens'],
+    type: ['Bug', 'Flying'],
+  }
+];
