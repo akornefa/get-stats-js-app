cc020d200b2cdd46f72ffa3464dd78ce281982be
Akornefa Akyea <akornefa@Akornefas-MBP.lan1>
2021-04-19 15:53:33 -0500

    Wrap pokemonList in  IIFE
    pokemonLIst array now within IIFE and given variable pokemonRepository. Updated forEach loop with new variable. in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,54 @@
+//ARRAY WRAPPED IN IIFE FUNCTION
+let pokemonRepository = (function() {
+  let pokemonList = [{
+      name: 'Charmeleon',
+      height: 1.1,
+      abilities: ['Blaze', ' Solar-power'],
+      type: ['Fire'],
+    },
+    {
+      name: 'Squirtle',
+      height: 0.5,
+      abilities: ['Rain-dish', ' Torrent'],
+      type: ['Water'],
+    },
+    {
+      name: 'Arbok',
+      height: 3.5,
+      abilities: ['Intimidate', ' Shed-skin', ' Unnerve'],
+      type: ['Poison'],
+    }
+  ];
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add(item) {
+    return pokemonList.push;
+  }
+
+  return {
+    getAll: getAll,
+    add: add
+  };
+
+})();
+
+//forEach Loop with pokemonRepository included
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+
+  if (pokemon.height > 1.5) {
+    document.write(pokemon.name + ' (height: ' + pokemon.height +
+      '). Wow, that\'s big! <br>')
+  } else {
+    document.write(pokemon.name + ' (height: ' + pokemon.height + '). <br>')
+  }
+});
+
+/*
+// ORIGINAL pokemonList Array
 let pokemonList = [{
     name: 'Charmeleon',
     height: 1.1,
@@ -17,6 +68,7 @@ let pokemonList = [{
     type: ['Poison'],
   }
 ];
+*/
 
 // For loop to print out all pokemons in Array. First if statement checks for height.
 /*for (let i = 0; i < pokemonList.length; i++) {
@@ -28,8 +80,3 @@ let pokemonList = [{
   }
 
 }*/
-
-pokemonList.forEach(function(pokemon){
-  document.write('Name: ' + pokemon.name + ', Height: ' + pokemon.height +
-  ', Abilities: (' + pokemon.abilities + '), and Type: ' + pokemon.type + '<br>');
-});
